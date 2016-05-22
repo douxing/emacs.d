334e5f521b1e8b9d51d4ffa0753b1e194af23d82
douxing <douxing1983@163.com>
2016-05-22 18:04:50 +0800

    init skeleton is okay

--- init.el
+++ init.el
@@ -5,12 +5,16 @@
 ;;
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-(defconst douxing-predefined/gc-cons-threshold gc-cons-threshold
+(defconst dx-predefined/gc-cons-threshold gc-cons-threshold
   "Initial value of 'gc-cons-threshold' at startup time")
 (setq gc-cons-threshold (* 128 1024 1024))
 ;; might like to uncomment these
 ;;(add-hook 'after-init-hook
-;;	  (lambda () (setq gc-cons-threshold douxing-predefined/gc-cons-threshold))
+;;	  (lambda () (setq gc-cons-threshold dx-predefined/gc-cons-threshold))
 
 (require 'init-site-lisp)
 (require 'init-elpa)
+
+;; next we are going to import the files
+(require 'init-elixir)
+(require 'init-ido)
--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -5,5 +5,11 @@
       '(;;("gnu" . "http://elpa.gnu.org/packages/")
 	("melpa" . "https://melpa.org/packages/")
 	("melpa-stable" . "https://stable.melpa.org/packages/")))
+(package-initialize)
+
+(defun dx-init-elpa/package-install (pkg)
+  "require package if not installed"
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
 
 (provide 'init-elpa)
