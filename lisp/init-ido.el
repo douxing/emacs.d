82b4a1e11d6494fbeac7a631b9684fe7c5b56fd9
douxing <douxing1983@163.com>
2016-05-30 06:37:26 +0800

    ensure packages

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -7,15 +7,18 @@
 	    (setq ido-enable-flex-matching t)))
 
 (use-package ido-ubiquitous
+  :ensure t
   :config (progn
 	    (ido-ubiquitous-mode t)))
 
 (use-package smex
+  :ensure t
   :init (progn
 	  (setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
 	  (smex-initialize))
   :bind ("M-x" . smex)) ;; or smex-major-mode-commands
 
-(dx-package-install 'idomenu)
+(use-package idomenu
+  :ensure t)
 
 (provide 'init-ido)
