48daf7ab22f013e6d9764e7c626083e8f3e5b9d5
douxing <douxing1983@163.com>
2016-05-30 01:41:48 +0800

    add ido-ubiquitous and idomenu for ido, js2-mode for js

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -1,4 +1,20 @@
 ;;
 (ido-mode t)
+(ido-everywhere t)
+(setq ido-enable-flex-matching t)
+
+(dx-package-install 'ido-ubiquitous)
+(require 'ido-ubiquitous)
+(ido-ubiquitous-mode t)
+
+(dx-package-install 'smex)
+(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+(smex-initialize)
+;; we can change between smex and smex-major-mode-commands
+;; but personally, i prefer smex
+(global-set-key [remap execute-extended-command] 'smex)
+;; (global-set-key [remap execute-extended-command] 'smex-major-mode-commands)
+
+(dx-package-install 'idomenu)
 
 (provide 'init-ido)
--- lisp/init-js.el
+++ lisp/init-js.el
@@ -1,3 +1,4 @@
 (dx-package-install 'coffee-mode)
+(dx-package-install 'js2-mode)
 
 (provide 'init-js)
