6d27abe728129a81db00216e9be724223dd836e8
dx <douxing@ceasia.com.cn>
2016-12-04 00:27:33 +0800

    add column for the file

--- init.el
+++ init.el
@@ -1,14 +1,4 @@
-;;
-;;
-;;
-
-;;
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+;;; begin of the file
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
@@ -22,9 +12,11 @@
 (require 'init-site-lisp)
 (require 'init-elpa)
 
+(require 'init-editing-utils)
+
 ;; next we are going to import the files
 (require 'init-ido)
 (require 'init-tools)
 (require 'init-languages)
 
-(global-auto-revert-mode t)
+;;; end of the file
---
+++ lisp/init-editing-utils.el
@@ -0,0 +1,4 @@
+(column-number-mode t)
+(global-auto-revert-mode t)
+
+(provide 'init-editing-utils)
