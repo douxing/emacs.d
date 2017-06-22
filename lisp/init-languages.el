2c5f12334a81d71776e18481036483e855e71590
douxing <doux060@zhixubao.com>
2017-06-22 11:26:56 +0800

    add padding in web-mode

--- lisp/init-languages.el
+++ lisp/init-languages.el
@@ -45,7 +45,10 @@
       (setq-default tab-width 2)
       (setq web-mode-code-indent-offset 2)
       (setq web-mode-markup-indent-offset 2)
-      (setq web-mode-css-indent-offset 2)))
+      (setq web-mode-css-indent-offset 2)
+      (setq web-mode-indent-style 2)
+      (setq web-mode-script-padding 2)
+      (setq web-mode-style-padding 2)))
 
 (use-package markdown-mode
   :ensure t
